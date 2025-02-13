dce2d46cf286dfbe3b3c9776be9e75e81224d670
Shivaji Shendge <shivajishendge2001@gmail.com>
2025-02-13 12:58:43 +0530

    creating session

---
+++ SessionFirst/src/main/java/com/ss/ValidateServer.java
@@ -0,0 +1,30 @@
+package com.ss;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "valid", urlPatterns = { "/valid" })
+public class ValidateServer extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		HttpSession session = request.getSession(true);
+		String sessionId = session.getId();
+		out.println("<h1>Hey User Your Session Id is "+ sessionId+"</h1>");
+		}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
